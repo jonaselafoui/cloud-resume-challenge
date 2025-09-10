1c0031c2d42a1945aac12b9fa0dbae40cf2dd206
Jonas Elafoui <jonas.elafoui@outlook.com>
2025-09-10 16:54:31 +0200

    added cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -246,3 +246,62 @@ resource "aws_codepipeline" "site_pipeline" {
 
   # Optional: add a manual approval stage, a deploy stage, etc.
 }
+
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                              = "s3_oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+locals {
+  s3_origin_id = "jonas-ma"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.jonas-ma.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = ["jonas.ma"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:535363408495:certificate/43d3a4fd-e5c7-429e-a200-db9c3a2e2efc"
+    ssl_support_method  = "sni-only"
+  }
+}
+
+//TODO: Add Route53
+//TODO: Set up automatic invalidation of the CloudFront cache after each deployment in CodeBuild
+//TODO: Add ACM certificate for the domain and use it in CloudFront
